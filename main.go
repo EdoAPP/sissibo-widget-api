05fcc0b557795cc603dc3d99fb0a1222ca858861
Eduardo Pacheco <eduardoapp.97@gmail.com>
2023-10-09 19:52:13 -0400

    Email integration with sendgrid

--- main.go
+++ main.go
@@ -5,30 +5,25 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"strings"
+
+	"github.com/joho/godotenv"
+	"github.com/sendgrid/sendgrid-go"
+	"github.com/sendgrid/sendgrid-go/helpers/mail"
 )
 
-var PORT int16 = 4321
+const PORT int16 = 4321
 
-type roastQty struct {
-	OneLb  int `json:"oneLb"`
-	TwoLb  int `json:"twoLb"`
-	FiveLb int `json:"fiveLb"`
-}
+type RoastName string
 
 type order struct {
-	CompanyName string              `json:"companyName"`
-	OrderNotes  string              `json:"orderNotes"`
-	OrderNumber string              `json:"orderNumber"`
-	RoastQty    map[string]roastQty `json:"roastQty"`
-}
-
-type response struct {
-	message string
-}
-
-func uploadImage(w http.ResponseWriter, r *http.Request) {
-
+	CompanyName      string  `json:"companyName"`
+	OrderNotes       string  `json:"orderNotes"`
+	OrderNumber      string  `json:"orderNumber"`
+	DeliveryLocation string  `json:"deliveryLocation,omitempty"`
+	OrderTotal       float64 `json:"orderTotal"`
+	OrderImage       string  `json:"orderImage"`
 }
 
 func submitOrder(w http.ResponseWriter, r *http.Request) {
@@ -40,16 +35,69 @@ func submitOrder(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Write([]byte("Order created succesfully"))
+	m := mail.NewV3Mail()
+
+	from := mail.NewEmail("Eduardo", "edo@shipyardsoftware.org") // Change to your verified sender
+	m.SetFrom(from)
+	m.SetTemplateID("d-a7ce9f0478ab48d3ae6cdfd4a043b3a7")
+
+	tos := []*mail.Email{
+		mail.NewEmail("Eduardo", "eduardoapp.97@gmail.com"),
+	}
+
+	p := mail.NewPersonalization()
+	p.AddTos(tos...)
+	p.SetDynamicTemplateData("Company_Name", order.CompanyName)
+	p.SetDynamicTemplateData("Delivery_Location", order.DeliveryLocation)
+	p.SetDynamicTemplateData("Purchase_Order", order.OrderNumber)
+	p.SetDynamicTemplateData("Order_Notes", order.OrderNotes)
+	p.SetDynamicTemplateData("Order_Total", order.OrderTotal)
+
+	m.AddPersonalizations(p)
+
+	m.AddAttachment(&mail.Attachment{
+		Content:     strings.Split(order.OrderImage, "base64,")[1],
+		Filename:    fmt.Sprintf("order-%v", order.CompanyName),
+		Type:        "image/png",
+		Disposition: "attachment",
+	})
+
+	// client := sendgrid.NewSendClient(os.Getenv("SENDGRID_API_KEY"))
+	// response, err := client.Send(m)
+
+	request := sendgrid.GetRequest(os.Getenv("SENDGRID_API_KEY"), "/v3/mail/send", "https://api.sendgrid.com")
+	request.Method = "POST"
+	var Body = mail.GetRequestBody(m)
+	request.Body = Body
+	response, err := sendgrid.API(request)
+
+	if err != nil {
+		fmt.Println(err)
+		http.Error(w, "Error while sending email", http.StatusInternalServerError)
+		return
+	} else {
+
+		fmt.Println(response.StatusCode)
+		fmt.Println(response.Body)
+		w.Write([]byte(response.Body))
+	}
+
 }
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	fmt.Println("Staring sever in port", PORT)
 
-	http.HandleFunc("/upload", uploadImage)
+	fmt.Println(
+		os.Getenv("SENDGRID_API_KEY"),
+	)
 	http.HandleFunc("/submit", submitOrder)
 
-	err := http.ListenAndServe(fmt.Sprintf(":%d", PORT), nil)
+	err = http.ListenAndServe(fmt.Sprintf(":%d", PORT), nil)
 	if err != nil {
 		log.Fatal(err)
 	}
